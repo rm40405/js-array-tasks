22f07f1d3ff6adddd4c2b8d4ff8615ecd034980b
rm40405 <rm404085@gmail.com>
2024-09-01 10:55:07 +0600

    js-array-tasks solved

---
+++ task5.js
@@ -0,0 +1,16 @@
+/*
+5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+*/
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+const combinedArray =array1.concat(array2);
+
+console.log(array1);
+console.log(combinedArray );
